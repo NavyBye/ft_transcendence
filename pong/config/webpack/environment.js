07f7ecc4094ca9965d404847b58f3b4421970dac
hyeyoo <50040414+hygoni@users.noreply.github.com>
2021-04-09 15:30:19 +0900

    Revert "프론트엔드 의존성 추가"

--- pong/config/webpack/environment.js
+++ pong/config/webpack/environment.js
@@ -1,25 +1,3 @@
-const { environment } = require('@rails/webpacker');
+const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack');
-
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default'],
-    _: 'underscore',
-    Backbone: 'backbone',
-    Marionette: 'backbone.marionette',
-  }),
-);
-
-environment.loaders.prepend('html', {
-  test: /\.html$/,
-  loader: 'underscore-template-loader',
-  query: {
-    prependFilenameComment: __dirname,
-  },
-});
-
-module.exports = environment;
+module.exports = environment
