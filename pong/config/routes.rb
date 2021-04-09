2faf28c000852c55032dd3c027b4b6449397957c
goyarn <goyarn1254@gmail.com>
2021-04-09 16:17:31 +0900

    [ADD] home#index for root

--- pong/config/routes.rb
+++ pong/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/index'
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   devise_scope :user do
     delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session_path
