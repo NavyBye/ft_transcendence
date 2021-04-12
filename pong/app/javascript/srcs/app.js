1d2f6bd2001ee1f960c250949cc2c8fa0b23bcf6
hygoni <9hrack@naver.com>
2021-04-12 15:10:16 +0900

    [ADD] add event based router

--- pong/app/javascript/srcs/app.js
+++ pong/app/javascript/srcs/app.js
@@ -1,12 +1,23 @@
 import Backbone from 'backbone';
+import Radio from 'backbone.radio';
 import view from './views';
+import Router from './router';
 
 const app = {
   start() {
-    if (Backbone.History.started) return;
-    Backbone.history.start();
+    /* Now you can get rootView using channel.request('rootView') */
+    const channel = Radio.channel('app');
     this.rootView = new view.RootView();
     this.rootView.render();
+    const root = this.rootView;
+    channel.reply('rootView', function getRootView() {
+      return root;
+    });
+
+    this.router = new Router();
+
+    if (Backbone.History.started) return;
+    Backbone.history.start();
   },
 };
 
