50ef9b764c4f0b32f30a46bac52a08f59b5673a7
hygoni <9hrack@naver.com>
2021-04-12 15:09:45 +0900

    [ADD] add resources for LoginView

---
+++ pong/app/javascript/srcs/views/LoginView.js
@@ -0,0 +1,9 @@
+import common from '../common';
+import template from '../templates/login.html';
+
+const LoginView = common.View.extend({
+  template,
+  onRender() {},
+});
+
+export default LoginView;
