d4853a540f16ff000e83e58790757fbef79dd6a4
hygoni <9hrack@naver.com>
2021-04-12 14:03:19 +0900

    [ADD] add event listener to NavView
    this will trigger route:<href> when clicking a link with <a href="<href>">

--- pong/app/javascript/srcs/views/NavView.js
+++ pong/app/javascript/srcs/views/NavView.js
@@ -1,7 +1,18 @@
+import Radio from 'backbone.radio';
 import common from '../common';
 import template from '../templates/nav.html';
 
+function link(event) {
+  const href = event.target.getAttribute('href');
+  const channel = Radio.channel('route');
+
+  event.preventDefault();
+  channel.trigger(`route:${href}`);
+}
+
 const NavView = common.View.extend({
+  el: '#nav',
+  events: { 'click a': link },
   template,
 });
 
