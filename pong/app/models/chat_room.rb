79472d6b15f3ad2cdc48ff0f179c46f2fde53f1c
DiceMono <hyekim@student.42seoul.kr>
2021-04-12 14:55:25 +0900

    [ADD] add model method

--- pong/app/models/chat_room.rb
+++ pong/app/models/chat_room.rb
@@ -1,4 +1,10 @@
 class ChatRoom < ApplicationRecord
-    validates :name, presence: true, uniqueness: true, length: { minimum: 1, maximum: 10 }
-    validates :password, length: { minimum: 4, maximum: 10 } # TODO minimum 조건 존재시 nil 입력가능한지?
+  validates :name, presence: true, uniqueness: true, length: { minimum: 1, maximum: 10 }
+  validates :password, length: { minimum: 4, maximum: 10 } # TODO minimum 조건 존재시 nil 입력가능한지?
+
+  attr_accessor :password
+
+  def public
+    self.password == nil
+  end
 end
