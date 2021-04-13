119f5ef1cdf564a928c708031324cad67ecd5d6e
goyarn <goyarn1254@gmail.com>
2021-04-13 11:58:11 +0900

    [ADD] action mailer settings

--- pong/config/environments/development.rb
+++ pong/config/environments/development.rb
@@ -35,6 +35,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  ## settings for mailer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: Rails.application.secrets.mail_id,
+    password: Rails.application.secrets.mail_pw,
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
