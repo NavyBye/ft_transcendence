ace599fda8cec53db3c2a99398b583057f12e9df
goyarn <goyarn1254@gmail.com>
2021-04-14 16:58:41 +0900

    [ADD] many fixtures

--- pong/test/models/user_test.rb
+++ pong/test/models/user_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:hyeyoo)
+  end
+
+  # nickname test
+  test 'valid with nickname newcomers duplicated' do
+    assert true
+  end
+
+  test 'invalid with nickname uniqueness' do
+    assert true
+  end
+
+  test 'valid with nickname stripped' do
+    assert true
+  end
+
+  # status test
+  test 'status should greater than -1' do
+    assert true
+  end
+
+  test 'status should less than 4' do
+    assert true
+  end
+
+  # rank test
+  test 'invalid rank pool' do
+    assert true
+  end
 end
