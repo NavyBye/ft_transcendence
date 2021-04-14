fc49ce07408a68284abb58123f382cd1cbefbb7b
goyarn <goyarn1254@gmail.com>
2021-04-14 19:32:04 +0900

    [ADD] user model unit tests

--- pong/test/models/user_test.rb
+++ pong/test/models/user_test.rb
@@ -2,33 +2,59 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = users(:hyeyoo)
+    @valid_user = users(:hyeyoo)
+  end
+
+  test 'valid normal case' do
+    assert @valid_user.valid?
   end
 
   # nickname test
-  test 'valid with nickname newcomers duplicated' do
-    assert true
+  test 'nickname uniqueness validations' do
+    @dupl_user = users(:hyeyoo_duplicated)
+    @dupl_user.nickname = @valid_user.nickname
+    assert_not @dupl_user.valid?
+
+    @dupl_user.nickname = "\r\n #{@valid_user.nickname} "
+    assert_not @dupl_user.valid?
+
+    # test with stripped
+    @valid_user.nickname = 'newcomer'
+    @dupl_user.nickname = @valid_user.nickname
+    assert @dupl_user.valid?
   end
 
-  test 'invalid with nickname uniqueness' do
-    assert true
-  end
+  test 'nickname length validations' do
+    @valid_user.nickname = 'aa'
+    assert @valid_user.valid?
+    @valid_user.nickname = 'aaaaaaaaaaaaaaaaaaaa'
+    assert @valid_user.valid?
 
-  test 'valid with nickname stripped' do
-    assert true
+    @valid_user.nickname = 'a'
+    assert_not @valid_user.valid?
+    @valid_user.nickname = 'aaaaaaaaaaaaaaaaaaaaa'
+    assert_not @valid_user.valid?
+    # test with stripped
+    @valid_user.nickname = " a \r \n "
+    assert_not @valid_user.valid?
   end
 
   # status test
-  test 'status should greater than -1' do
-    assert true
+  test 'status validations' do
+    assert_raises ArgumentError do
+      @valid_user.status = -1
+    end
+    assert_raises ArgumentError do
+      @valid_user.status = 4
+    end
+    (0..3).each do |i|
+      @valid_user.status = i
+      assert @valid_user.valid?
+    end
   end
 
-  test 'status should less than 4' do
-    assert true
-  end
-
-  # rank test
-  test 'invalid rank pool' do
-    assert true
+  test 'trophy validation' do
+    @valid_user.trophy = -1
+    assert_not @valid_user.valid?
   end
 end
