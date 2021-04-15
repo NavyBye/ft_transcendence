383c78dbe6df9a8de1df72db1607bc42a5306845
DiceMono <hyekim@student.42seoul.kr>
2021-04-15 14:19:03 +0900

    [REFACTOR] refactor

--- pong/config/routes.rb
+++ pong/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace 'api' do
-    resources :chat_rooms, path: 'chatrooms', only: [:index, :update, :destroy, :create] do
-
+    resources :chat_rooms, path: 'chatrooms', only: %i[index update destroy create] do
     end
   end
+
+  get "/", to: "hello#index"
 end
