2e88d4b84acf9cb50b44e0877bdffb88b4ebdf81
goyarn <goyarn1254@gmail.com>
2021-04-15 17:58:45 +0900

    [FIX] routes minor bug

--- pong/config/routes.rb
+++ pong/config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'friends/index'
-    get 'friends/create'
-    get 'friends/destroy'
-  end
   root 'home#index'
-  get 'home/index'
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   devise_scope :user do
     delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session_path
