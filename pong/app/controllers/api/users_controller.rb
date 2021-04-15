9f51ff801c37924abd061f473bf5e7c4d1f65bdb
goyarn <goyarn1254@gmail.com>
2021-04-15 15:08:49 +0900

    [ADD] carrierwave applied to controller & configuration

--- pong/app/controllers/api/users_controller.rb
+++ pong/app/controllers/api/users_controller.rb
@@ -2,11 +2,11 @@ module Api
   class UsersController < ApplicationController
     before_action :authenticate_user!
     def index
-      render json: User.all
+      render json: User.all, status: ok
     end
 
     def rank
-      render json: User.all.order(rating: :desc).page(params[:page])
+      render json: User.all.order(rating: :desc).page(params[:page]), status: ok
     end
 
     def show
@@ -28,7 +28,7 @@ module Api
 
     def update_params
       params.require(:id)
-            .permit(:nickname, :is_banned, :is_email_auth)
+            .permit(:nickname, :is_banned, :is_email_auth, :image)
     end
   end
 end
