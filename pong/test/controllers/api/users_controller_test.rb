ab009416da7745b1de940ebb8f05a56e0c396254
goyarn <goyarn1254@gmail.com>
2021-04-15 14:15:14 +0900

    [ADD] user api a few simple tests with devise helper

--- pong/test/controllers/api/users_controller_test.rb
+++ pong/test/controllers/api/users_controller_test.rb
@@ -2,8 +2,37 @@ require "test_helper"
 
 module Api
   class UsersControllerTest < ActionDispatch::IntegrationTest
-    # test "the truth" do
-    #   assert true
-    # end
+    include Devise::Test::IntegrationHelpers
+
+    test 'users index' do
+      user = users(:hyeyoo)
+      sign_in user
+      get '/api/users'
+      assert_response :success
+    end
+
+    test 'users index without login' do
+      get '/api/users'
+      assert_response :redirect
+    end
+
+    test 'user show' do
+      user = users(:hyeyoo)
+      sign_in user
+      get '/api/users/1'
+      assert_response :success
+    end
+
+#    test 'user show not found' do
+#      user = users(:hyeyoo)
+#      sign_in user
+#      get '/api/users/0'
+#      assert_response :not_found
+#    end
+
+    test 'user show without login' do
+      get '/api/users/1'
+      assert_response :redirect
+    end
   end
 end
