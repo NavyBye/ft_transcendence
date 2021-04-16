e1b972ee88dbff79ddcea2164df1263e7a2e5d68
goyarn <goyarn1254@gmail.com>
2021-04-16 15:13:14 +0900

    [ADD] self_follow validation

--- pong/app/models/friend.rb
+++ pong/app/models/friend.rb
@@ -1,4 +1,14 @@
 class Friend < ApplicationRecord
   belongs_to :user, class_name: "User"
   belongs_to :follow, class_name: "User"
+
+  validate :cannot_self_follow
+
+  private
+
+  def cannot_self_follow
+    if user_id == follow_id
+      errors.add(:follow_id, 'you cannot follow yourself.')
+    end
+  end
 end
--- pong/test/models/friend_test.rb
+++ pong/test/models/friend_test.rb
@@ -4,4 +4,14 @@ class FriendTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = users(:hyeyoo)
+    @follow = users(:hyekim)
+  end
+  test 'friend validate' do
+    @friendship = Friend.create(user_id: @user.id, follow_id: @follow.id)
+    assert @friendship.valid?
+    @self_follow = Friend.create(user_id: @user.id, follow_id: @user.id)
+    assert_not @self_follow.valid?
+  end
 end
