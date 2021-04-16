8c1cef2aebd47be81dfdd4692db9181f7c314a99
goyarn <goyarn1254@gmail.com>
2021-04-16 15:50:10 +0900

    [UPDATE] add 2 column indexing to friends/blocks table

--- pong/db/schema.rb
+++ pong/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_061746) do
+ActiveRecord::Schema.define(version: 2021_04_16_064659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_04_16_061746) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["blocked_user_id"], name: "index_blocks_on_blocked_user_id"
+    t.index ["user_id", "blocked_user_id"], name: "index_blocks_on_user_id_and_blocked_user_id", unique: true
     t.index ["user_id"], name: "index_blocks_on_user_id"
   end
 
@@ -38,6 +39,7 @@ ActiveRecord::Schema.define(version: 2021_04_16_061746) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["follow_id"], name: "index_friends_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_friends_on_user_id_and_follow_id", unique: true
     t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
