18d33e7d5218aa04c71a83a1a56e5b656d338c5b
goyarn <goyarn1254@gmail.com>
2021-04-16 14:30:25 +0900

    [FIX] delete weird controller tests

--- pong/test/controllers/api/friends_controller_test.rb
+++ pong/test/controllers/api/friends_controller_test.rb
@@ -3,18 +3,6 @@ require "test_helper"
 module Api
   class FriendsControllerTest < ActionDispatch::IntegrationTest
     test "should get index" do
-      get api_friends_index_url
-      assert_response :success
-    end
-
-    test "should get create" do
-      get api_friends_create_url
-      assert_response :success
-    end
-
-    test "should get destroy" do
-      get api_friends_destroy_url
-      assert_response :success
     end
   end
 end
