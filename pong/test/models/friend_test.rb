bf0a23c488b824c027a9fd2c2baf2a5caa7aa7d3
goyarn <goyarn1254@gmail.com>
2021-04-16 15:41:05 +0900

    [ADD] unique combination validate

--- pong/test/models/friend_test.rb
+++ pong/test/models/friend_test.rb
@@ -11,6 +11,10 @@ class FriendTest < ActiveSupport::TestCase
   test 'friend validate' do
     @friendship = Friend.create(user_id: @user.id, follow_id: @follow.id)
     assert @friendship.valid?
+
+    @friendship = Friend.create(user_id: @user.id, follow_id: @follow.id)
+    assert_not @friendship.valid?
+
     @self_follow = Friend.create(user_id: @user.id, follow_id: @user.id)
     assert_not @self_follow.valid?
   end
