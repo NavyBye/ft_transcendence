e1a3ebd737c95c90c1cf1c270dc2c22fb27eba90
hygoni <9hrack@naver.com>
2021-04-19 20:43:07 +0900

    [ADD] add login (only 42 login)
    TODO: implement custom login

--- pong/app/javascript/srcs/app.js
+++ pong/app/javascript/srcs/app.js
@@ -1,23 +1,40 @@
 import Backbone from 'backbone';
+import $ from 'jquery/src/jquery';
 import Radio from 'backbone.radio';
 import view from './views';
 import Router from './router';
 
 const app = {
   start() {
-    /* Now you can get rootView using channel.request('rootView') */
-    const channel = Radio.channel('app');
-    this.rootView = new view.RootView();
-    this.rootView.render();
-    const root = this.rootView;
-    channel.reply('rootView', function getRootView() {
-      return root;
+    Radio.channel('app').reply('logout', function logout() {
+      app.user = null;
     });
 
-    this.router = new Router();
+    /* reply user */
+    Radio.channel('app').reply('login', function getUser() {
+      return app.user;
+    });
 
-    if (Backbone.History.started) return;
-    Backbone.history.start();
+    Promise.all([
+      /* get userinfo */
+      $.ajax({
+        type: 'GET',
+        url: '/api/users/me',
+        success(data) {
+          app.user = data;
+        },
+      }),
+    ]).finally(function then() {
+      app.router = new Router();
+      /* reply rootView */
+      app.rootView = new view.RootView();
+      Radio.channel('app').reply('rootView', function getRootView() {
+        return app.rootView;
+      });
+      app.rootView.render();
+      if (!Backbone.History.started) Backbone.history.start();
+      app.router.navigate(Backbone.history.fragment, { trigger: true });
+    });
   },
 };
 
--- pong/app/javascript/srcs/views/NavView.js
+++ pong/app/javascript/srcs/views/NavView.js
@@ -1,18 +1,30 @@
 import Radio from 'backbone.radio';
+import $ from 'jquery/src/jquery';
 import common from '../common';
 import template from '../templates/NavView.html';
 
 function link(event) {
   const href = event.target.getAttribute('href');
-  const channel = Radio.channel('route');
 
   event.preventDefault();
-  channel.trigger('route', href);
+  Radio.channel('route').trigger('route', href);
+}
+
+function logout(event) {
+  Radio.channel('app').request('logout');
+  event.preventDefault();
+  $.ajax({
+    type: 'DELETE',
+    url: '/sign_out',
+    success() {
+      Radio.channel('route').trigger('route', 'login');
+    },
+  });
 }
 
 const NavView = common.View.extend({
   el: '#nav',
-  events: { 'click a': link },
+  events: { 'click #logout': logout, 'click a:not(#logout)': link },
   template,
 });
 
