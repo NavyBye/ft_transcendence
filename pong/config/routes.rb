b85501b9ab7e1f2142201ece1a3083c3cb015de7
goyarn <goyarn1254@gmail.com>
2021-04-21 17:17:37 +0900

    [ADD] add devise controller to config devise registration action

--- pong/config/routes.rb
+++ pong/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks",
+    registrations: "users/registrations"
+  }
   devise_scope :user do
     delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session_path
   end
