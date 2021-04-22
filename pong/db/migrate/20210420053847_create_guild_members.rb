fb3c9aa9b864707cbe63a995333bb416ee27925d
goyarn <goyarn1254@gmail.com>
2021-04-23 01:34:51 +0900

    [FIX] guild_id should not be unique.

--- pong/db/migrate/20210420053847_create_guild_members.rb
+++ pong/db/migrate/20210420053847_create_guild_members.rb
@@ -2,7 +2,7 @@ class CreateGuildMembers < ActiveRecord::Migration[6.1]
   def change
     create_table :guild_members do |t|
       t.references :user, null: false, foreign_key: { to_table: :users }, index: { unique: true }
-      t.references :guild, null: false, foreign_key: { to_table: :guilds }, index: { unique: true }
+      t.references :guild, null: false, foreign_key: { to_table: :guilds }
       t.integer :role, null: false, default: 0
       t.timestamps
     end
