15791ec127f3863a6c979caf13738da6acaa43e5
hygoni <9hrack@naver.com>
2021-04-22 16:53:44 +0900

    [UPDATE] update auth utility get token from cookie

--- pong/app/javascript/srcs/utils/auth.js
+++ pong/app/javascript/srcs/utils/auth.js
@@ -2,10 +2,26 @@ import $ from 'jquery/src/jquery';
 
 const auth = {
   getTokenKey() {
-    return $('meta[name=csrf-param]').attr('content');
+    const originalToken = $('meta[name=csrf-param]').attr('content');
+    const myToken = auth.getCookie('my_csrf_token');
+    console.log(`my token${myToken}`);
+    return myToken || originalToken;
   },
   getTokenValue() {
     return $('meta[name=csrf-token]').attr('content');
+  },
+  getCookie(name) {
+    const cookieName = `${name}=`;
+    const cookieData = document.cookie;
+    let start = cookieData.indexOf(cookieName);
+    let cookieValue = '';
+    if (start !== -1) {
+      start += cookieName.length;
+      let end = cookieData.indexOf(';', start);
+      if (end === -1) end = cookieData.length;
+      cookieValue = cookieData.substring(start, end);
+    }
+    return unescape(cookieValue);
   },
 };
 
