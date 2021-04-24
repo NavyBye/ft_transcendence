a6a3c81c2790825c041fcb627cc592f3c8c6395b
kms <kmslkh@naver.com>
2021-04-24 14:49:33 +0900

    [UPDATE] chat send기능 구현

--- pong/app/javascript/srcs/views/ChatCollectionView.js
+++ pong/app/javascript/srcs/views/ChatCollectionView.js
@@ -1,8 +1,11 @@
+/* eslint-disable prefer-destructuring */
 import $ from 'jquery/src/jquery';
 import collection from '../collections';
 import common from '../common';
 import template from '../templates/ChatCollectionView.html';
 import ChatView from './ChatView';
+import consumer from '../../channels/consumer';
+import model from '../models';
 
 const ChatCollectionView = common.CollectionView.extend({
   el: '#side .content',
@@ -10,10 +13,50 @@ const ChatCollectionView = common.CollectionView.extend({
   childContainer: '#chat-collection',
   ViewType: ChatView,
   CollectionType: collection.ChatCollection,
-  onInitialize() {},
-  onRender() {},
+  events: {
+    'click #input-chat .submit': 'sendMsg',
+    'keypress #input-chat input': 'sendMsgEnter',
+  },
+  onInitialize() {
+    window.comsumer = consumer;
+    const chatRoomId = this.collection.chatRoomId;
+    const view = this;
+    this.channel = consumer.subscriptions.create(
+      {
+        channel: 'ChatRoomChannel',
+        id: chatRoomId,
+      },
+      {
+        connected() {},
+        disconnected() {},
+        received(data) {
+          const newModel = new model.ChatModel(JSON.parse(data.data));
+          view.add(newModel);
+        },
+      },
+    );
+  },
+  onDestroy() {
+    this.channel.unsubscribe();
+  },
   afterAdd() {
     $('#chat-collection').scrollTop($('#chat-collection')[0].scrollHeight);
+  },
+  sendMsg() {
+    const body = $('#input-chat input').val();
+    if (body.length > 0) {
+      $('#input-chat input').val('');
+      this.channel.send({ body });
+    }
+  },
+  sendMsgEnter(event) {
+    if (event.which === 13) {
+      const body = $('#input-chat input').val();
+      if (body.length > 0) {
+        $('#input-chat input').val('');
+        this.channel.send({ body });
+      }
+    }
   },
 });
 
