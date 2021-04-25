9b0fe8db88381a9ce50f23af78c65898ca03e8c8
goyarn <goyarn1254@gmail.com>
2021-04-25 23:30:53 +0900

    [ADD] model test base

--- pong/test/models/invite_test.rb
+++ pong/test/models/invite_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class InviteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @invite_one = invites(:testguild_to_hyekim)
+    @invite_two = invites(:test2_to_hyekim)
+  end
+
+  test "duplicated invite" do
+    testguild = @invite_one.guild
+    hyekim = @invite_one.user
+    new_invite = Invite.new(guild_id: testguild.id, user_id: hyekim.id)
+    assert_not new_invite.valid?
+  end
+
+  # TODO : invite to someone who already have guild
+
 end
