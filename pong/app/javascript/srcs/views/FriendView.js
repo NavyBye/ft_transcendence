6e5d2db0c98a5a4d3439dfb14eaec3c734505c90
Hyeonggon Yoo <42.hyeyoo@gmail.com>
2021-04-26 20:38:52 +0900

    [ADD] add friend remove button

--- pong/app/javascript/srcs/views/FriendView.js
+++ pong/app/javascript/srcs/views/FriendView.js
@@ -1,9 +1,29 @@
+import { Radio } from 'backbone';
+import BootstrapMenu from 'bootstrap-menu';
+import $ from 'jquery/src/jquery';
 import common from '../common';
 import template from '../templates/FriendView.html';
 
 const FriendView = common.View.extend({
   template,
-  onRender() {},
+  onRender() {
+    const id = this.model.get('id');
+    const login = Radio.channel('app').request('login');
+    this.menu = new BootstrapMenu(`.friend[id=${id}]`, {
+      actions: [
+        {
+          name: 'Destroy',
+          onClick() {
+            $.ajax({
+              type: 'DELETE',
+              url: `/api/users/${login.id}/friends/${id}`,
+            });
+          },
+          classNames: 'dropdown-item',
+        },
+      ],
+    });
+  },
 });
 
 export default FriendView;
