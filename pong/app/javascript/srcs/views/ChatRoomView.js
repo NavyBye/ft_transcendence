ae1786b2119684f4a556125b56af07465a6b1c9d
Hyeonggon Yoo <42.hyeyoo@gmail.com>
2021-04-26 15:51:17 +0900

    [ADD] add Destroy contextmenu in ChatRoomCollectionView

--- pong/app/javascript/srcs/views/ChatRoomView.js
+++ pong/app/javascript/srcs/views/ChatRoomView.js
@@ -1,3 +1,6 @@
+/* eslint-disable prefer-destructuring */
+import $ from 'jquery/src/jquery';
+import BootstrapMenu from 'bootstrap-menu';
 import Radio from 'backbone.radio';
 import common from '../common';
 import template from '../templates/ChatRoomView.html';
@@ -7,6 +10,23 @@ const ChatRoomView = common.View.extend({
   // onRender() {},
   events: {
     'click .enter-room': 'enterRoom',
+  },
+  onRender() {
+    const id = this.model.get('id');
+    this.menu = new BootstrapMenu(`.chatroom[room-id=${id}]`, {
+      actions: [
+        {
+          name: 'Destroy',
+          onClick() {
+            $.ajax({
+              type: 'DELETE',
+              url: `/api/chatrooms/${id}`,
+            });
+          },
+          classNames: 'dropdown-item',
+        },
+      ],
+    });
   },
   enterRoom() {
     const chatRoomId = this.model.get('id');
