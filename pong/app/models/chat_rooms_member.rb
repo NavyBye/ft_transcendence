4f589fc77cc395874d21180232be76ff1fbce9a1
DiceMono <hyekim@student.42seoul.kr>
2021-04-26 20:29:12 +0900

    [ADD] add error class for ChatRoomsMember

--- pong/app/models/chat_rooms_member.rb
+++ pong/app/models/chat_rooms_member.rb
@@ -1,7 +1,10 @@
 class ChatRoomsMember < ApplicationRecord
+  class PermissionDenied < StandardError; end
+
   belongs_to :chat_room
   belongs_to :user
 
+  validates :user_id, uniqueness: { scope: :chat_room_id }
   enum status: { normal: 0, muted: 1, banned: 2 }
   enum role: { user: 0, admin: 1, owner: 2 }
 end
