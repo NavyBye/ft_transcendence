124519dc6478e92fcb5bda33b81bd16849c750f9
DiceMono <hyekim@student.42seoul.kr>
2021-04-28 14:07:46 +0900

    [ADD] add migration for unique index

--- pong/db/schema.rb
+++ pong/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_051209) do
+ActiveRecord::Schema.define(version: 2021_04_28_045251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_051209) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["chat_room_id"], name: "index_chat_rooms_members_on_chat_room_id"
+    t.index ["user_id", "chat_room_id"], name: "index_chat_rooms_members_on_user_id_and_chat_room_id", unique: true
     t.index ["user_id"], name: "index_chat_rooms_members_on_user_id"
   end
 
