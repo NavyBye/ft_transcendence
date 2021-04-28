61e05ace947ef3843990104ae213f55ca7bff68d
DiceMono <hyekim@student.42seoul.kr>
2021-04-28 14:08:11 +0900

    [UPDATE] update job and rename

--- pong/app/jobs/chat_rooms_member_update_status_job.rb
+++
@@ -1,7 +0,0 @@
-class ChatRoomsMemberUpdateStatusJob < ApplicationJob
-  queue_as :default
-
-  def perform(chat_rooms_member_id, status)
-    ChatRoomsMember.update chat_rooms_member_id, :status => status
-  end
-end
