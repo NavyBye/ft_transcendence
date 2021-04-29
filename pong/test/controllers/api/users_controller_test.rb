008c1f3d2459414d1d6a76f4071035d8bbfe72a0
goyarn <goyarn1254@gmail.com>
2021-04-29 14:06:19 +0900

    [ADD] image update test 1

--- pong/test/controllers/api/users_controller_test.rb
+++ pong/test/controllers/api/users_controller_test.rb
@@ -6,8 +6,7 @@ module Api
     include Devise::Test::IntegrationHelpers
 
     test 'users index' do
-      user = users(:hyeyoo)
-      sign_in user
+      login :hyeyoo
       get '/api/users'
       assert_response :success
     end
@@ -18,15 +17,13 @@ module Api
     end
 
     test 'user show' do
-      user = users(:hyeyoo)
-      sign_in user
-      get "/api/users/#{user.id}"
+      login :hyeyoo
+      get "/api/users/#{@user.id}"
       assert_response :success
     end
 
     test 'user show not found' do
-      user = users(:hyeyoo)
-      sign_in user
+      login :hyeyoo
       get '/api/users/0'
       assert_response :missing
     end
@@ -37,11 +34,30 @@ module Api
     end
 
     test 'user update' do
-      @user = users(:hyeyoo)
-      sign_in @user
-      put api_user_path(@user.id), params: { nickname: 'new_nickname' }
+      login :hyeyoo
+      assert_changes '@user.reload.nickname' do
+        put api_user_path(@user.id), params: { nickname: 'new_nickname' }
+      end
       result = JSON.parse @response.body
       assert_equal result['nickname'], 'new_nickname'
+    end
+
+    test 'user image update' do
+      login :user_without_image
+      assert_changes '@user.reload.image' do
+        file_dir = Rails.root.join('test/fixtures/files/')
+        File.open(file_dir.join('rails_logo.png')) do |image_file|
+          put api_user_path(@user.id), params: { image: image_file }
+        end
+        assert_response :ok
+      end
+    end
+
+    private
+
+    def login(user_symbol)
+      @user = users(user_symbol)
+      sign_in @user
     end
   end
 end
