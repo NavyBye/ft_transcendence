96ac660622a9d976629b4a330ed00543dc493eb1
DiceMono <hyekim@student.42seoul.kr>
2021-04-30 14:55:48 +0900

    [UPDATE] update basic associations

--- pong/app/models/dm_room.rb
+++ pong/app/models/dm_room.rb
@@ -1,2 +1,5 @@
 class DmRoom < ApplicationRecord
+  has_many :dm_rooms_members, dependent: :destroy
+  has_many :members, through: :dm_rooms_members, source: :user
+  has_many :messages, dependent: :destroy, class_name: "DmRoomMessage"
 end
--- pong/app/models/dm_rooms_member.rb
+++ pong/app/models/dm_rooms_member.rb
@@ -1,2 +1,6 @@
 class DmRoomsMember < ApplicationRecord
+  belongs_to :dm_room
+  belongs_to :user
+
+  validates :user_id, uniqueness: { scope: :dm_room_id }
 end
