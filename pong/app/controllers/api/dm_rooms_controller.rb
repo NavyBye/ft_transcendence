7b51b176acc90e741014f7634acc39d78297a2c1
DiceMono <hyekim@student.42seoul.kr>
2021-04-30 17:39:58 +0900

    [ADD] add dm romm controller

---
+++ pong/app/controllers/api/dm_rooms_controller.rb
@@ -0,0 +1,20 @@
+module Api
+  class DmRoomsController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+      render json: serialize(current_user.dm_rooms.all), status: :ok
+    end
+
+    def create
+      current_user.dm_rooms.each do |dm_room|
+        if dm_room.members.exists? user_id: params[:user_id]
+          return render json: serialize(dm_room), status: :ok
+        end
+      end
+      dm_room = DmRoom.create!
+      dm_room.members << [current_user, User.find params[:user_id]]
+      render json: serialize(dm_room), status: :created
+    end
+  end
+end
