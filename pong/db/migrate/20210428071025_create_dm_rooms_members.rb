193926957e66c58e7c7fddbf837601f98a680b33
DiceMono <hyekim@student.42seoul.kr>
2021-04-30 14:53:29 +0900

    [FIX] fix err about table name

--- pong/db/migrate/20210428071025_create_dm_rooms_members.rb
+++ pong/db/migrate/20210428071025_create_dm_rooms_members.rb
@@ -1,10 +1,10 @@
 class CreateDmRoomsMembers < ActiveRecord::Migration[6.1]
   def change
     create_table :dm_rooms_members do |t|
-      t.references :dm_room, null: false, foreign_key: { to_table: :chat_rooms, on_delete: :cascade }
+      t.references :dm_room, null: false, foreign_key: { to_table: :dm_rooms, on_delete: :cascade }
       t.references :user, null: false, foreign_key: { to_table: :users, on_delete: :cascade }
       t.boolean :exited, default: false
-      t.index %i[user_id chat_room_id], unique: true
+      t.index %i[user_id dm_room_id], unique: true
 
       t.timestamps
     end
--- pong/db/migrate/20210428071031_create_dm_room_messages.rb
+++ pong/db/migrate/20210428071031_create_dm_room_messages.rb
@@ -1,7 +1,7 @@
 class CreateDmRoomMessages < ActiveRecord::Migration[6.1]
   def change
     create_table :dm_room_messages do |t|
-      t.references :dm_room, null: false, foreign_key: { to_table: :chat_rooms, on_delete: :cascade }
+      t.references :dm_room, null: false, foreign_key: { to_table: :dm_rooms, on_delete: :cascade }
       t.references :user, null: false, foreign_key: { to_table: :users, on_delete: :cascade }
       t.string :body
 
