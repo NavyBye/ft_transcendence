c26b0d68719d5a980a127f3389792a55d56d10dd
DiceMono <hyekim@student.42seoul.kr>
2021-04-30 22:52:11 +0900

    [REFACTOR] refactor by rubocop

--- pong/db/migrate/20210428070949_create_dm_rooms.rb
+++ pong/db/migrate/20210428070949_create_dm_rooms.rb
@@ -1,8 +1,5 @@
 class CreateDmRooms < ActiveRecord::Migration[6.1]
   def change
-    create_table :dm_rooms do |t|
-
-      t.timestamps
-    end
+    create_table :dm_rooms, &:timestamps
   end
 end
