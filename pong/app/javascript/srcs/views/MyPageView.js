3f892feaed6dd88972b09c13e02ce77b1c977a93
Hyeonggon Yoo <42.hyeyoo@gmail.com>
2021-05-01 17:37:03 +0900

    [UPDATE] now user can upload profile image

--- pong/app/javascript/srcs/views/MyPageView.js
+++ pong/app/javascript/srcs/views/MyPageView.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-destructuring */
 import $ from 'jquery/src/jquery';
 import common from '../common';
 import template from '../templates/MyPageView.html';
@@ -16,31 +17,34 @@ const MyPageView = common.View.extend({
     const enable2FA = $('#my-page input[name=2fa]').is(':checked');
     const file = $('#my-page #upload-image').get(0).files[0];
 
-    if (file) {
-      const formData = new FormData();
-      formData.append('file', file);
-      /* image upload */
-      $.ajax({
-        type: 'POST',
-        url: /* I don't know what url is */ '',
-        data: formData,
-        enctype: 'multipart/form-data',
-        processData: false,
-        contentType: false,
-        error(res) {
-          new ErrorModalView().show(
-            'Error while uploading image',
-            res.responseText,
-          );
-        },
-      });
-    }
-
     this.model.set('nickname', nickname);
     this.model.set('is_email_auth', enable2FA);
-    this.model.save({
-      /* FIXME: callback is not working */
+
+    const formData = new FormData();
+    if (file) {
+      formData.append('image', file);
+      /* image preview */
+      const reader = new FileReader();
+      reader.onload = function onload(e) {
+        $('#my-page img').attr('src', e.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+
+    formData.append('nickname', nickname);
+    formData.append('is_email_auth', enable2FA);
+
+    /* upload */
+    const self = this;
+    $.ajax({
+      type: 'PUT',
+      url: `/api/users/${this.model.get('id')}`,
+      data: formData,
+      enctype: 'multipart/form-data',
+      processData: false,
+      contentType: false,
       success() {
+        self.model.fetch();
         new OkModalView().show('Success', 'Successfully saved user data');
       },
       error(res) {
@@ -50,7 +54,6 @@ const MyPageView = common.View.extend({
         );
       },
     });
-    this.model.fetch();
   },
 });
 
