5bd09514afadeb715e61c448114d3837adaef33f
goyarn <goyarn1254@gmail.com>
2021-05-04 20:49:00 +0900

    [ADD] broadcast status when login/out

--- pong/app/controllers/users/omniauth_callbacks_controller.rb
+++ pong/app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,6 +5,7 @@ module Users
 
       if @user.persisted?
         sign_in_and_redirect @user, event: :authentication
+        @user.session_create
         # sign_in @user, event: :authentication
         # set_flash_message(:notice, :success, kind: "42") if is_navigational_format?
       else
