627f88e19a1597616a82e1c686cb4f3e6d5a9d75
goyarn <goyarn1254@gmail.com>
2021-05-06 19:21:24 +0900

    [ADD] associations and model basic validations

--- pong/app/models/game.rb
+++ pong/app/models/game.rb
@@ -1,2 +1,18 @@
 class Game < ApplicationRecord
+  # enums
+  enum game_type: {
+    duel: 0,
+    ladder: 1,
+    ladder_tournament: 2,
+    tournament: 3,
+    war: 4,
+    friendly: 5
+  }
+
+  # associations
+  has_many :game_players, class_name: "GamePlayer", foreign_key: :game_id, inverse_of: :game, dependent: :destroy
+  has_many :players, through: :game_players, source: :user
+
+  # validations
+  validates :game_type, inclusion: { in: Game.game_types.keys }
 end
--- pong/app/models/game_player.rb
+++ pong/app/models/game_player.rb
@@ -1,2 +1,8 @@
 class GamePlayer < ApplicationRecord
+  # associations
+  belongs_to :user, class_name: "User", foreign_key: :user_id, inverse_of: :game_relation
+  belongs_to :game, class_name: "Game", foreign_key: :game_id, inverse_of: :game_players
+
+  # validations
+  validates :user_id, uniqueness: true
 end
