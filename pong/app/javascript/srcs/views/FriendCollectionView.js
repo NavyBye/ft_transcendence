57dd9f5216a62c316525d01a92bb01ddf1c7fd7f
kms <kmslkh@naver.com>
2021-05-07 00:37:30 +0900

    [FIX] change subscribe collectionView->View

--- pong/app/javascript/srcs/views/FriendCollectionView.js
+++ pong/app/javascript/srcs/views/FriendCollectionView.js
@@ -2,32 +2,13 @@ import collection from '../collections';
 import common from '../common';
 import template from '../templates/FriendCollectionView.html';
 import FriendView from './FriendView';
-import consumer from '../../channels/consumer';
 
 const FriendCollectionView = common.CollectionView.extend({
   template,
   childContainer: '#friend-collection',
   ViewType: FriendView,
   CollectionType: collection.FriendCollection,
-  afterAdd(m) {
-    this.channel = consumer.subscriptions.create(
-      {
-        channel: 'FriendChannel',
-        id: m.get('id'),
-      },
-      {
-        connected() {
-          console.log('tt');
-        },
-        disconnected() {
-          console.log('dis con');
-        },
-        received(data) {
-          console.log(data);
-        },
-      },
-    );
-  },
+  afterAdd() {},
   onRender() {},
 });
 
--- pong/app/javascript/srcs/views/FriendView.js
+++ pong/app/javascript/srcs/views/FriendView.js
@@ -4,9 +4,27 @@ import $ from 'jquery/src/jquery';
 import common from '../common';
 import template from '../templates/FriendView.html';
 import auth from '../utils/auth';
+import consumer from '../../channels/consumer';
 
 const FriendView = common.View.extend({
   template,
+  onInitialize() {
+    const self = this;
+    this.channel = consumer.subscriptions.create(
+      {
+        channel: 'FriendChannel',
+        id: self.model.get('id'),
+      },
+      {
+        connected() {},
+        disconnected() {},
+        received(data) {
+          self.model.set({ status: JSON.parse(data.data).status });
+          self.render();
+        },
+      },
+    );
+  },
   onRender() {
     const id = this.model.get('id');
     const login = Radio.channel('login').request('get');
