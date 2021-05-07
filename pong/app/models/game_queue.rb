2f51e7d451f80abe3ad2321fb22c86924f0b1ec8
goyarn <goyarn1254@gmail.com>
2021-05-07 13:11:17 +0900

    [ADD] enum to game_queue

--- pong/app/models/game_queue.rb
+++ pong/app/models/game_queue.rb
@@ -1,4 +1,7 @@
 class GameQueue < ApplicationRecord
+  # enum
+  enum game_type: Game.game_types
+
   # validations
   validates :game_type, inclusion: { in: Game.game_types.keys }
   validates :user_id, uniqueness: true
