5a1dbb11d2f2a8233a7126abc0b7000a352e2211
kms <kmslkh@naver.com>
2021-05-08 02:02:13 +0900

    [ADD] add guild-manage view, template

---
+++ pong/app/javascript/srcs/views/GuildManageView.js
@@ -0,0 +1,9 @@
+import common from '../common';
+import template from '../templates/GuildManageView.html';
+
+const GuildManageView = common.View.extend({
+  template,
+  onRender() {},
+});
+
+export default GuildManageView;
