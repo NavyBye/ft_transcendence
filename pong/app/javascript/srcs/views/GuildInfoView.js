aa88087ac7e25b8a6c90c5f1d6a3c3517e1af951
kms <kmslkh@naver.com>
2021-05-08 18:36:52 +0900

    [UPDATE] add Leave Guild button

--- pong/app/javascript/srcs/views/GuildInfoView.js
+++ pong/app/javascript/srcs/views/GuildInfoView.js
@@ -1,9 +1,17 @@
+import $ from 'jquery/src/jquery';
+import Radio from 'backbone.radio';
 import common from '../common';
+import auth from '../utils/auth';
 import template from '../templates/GuildInfoView.html';
 import GuildMemberCollectionView from './GuildMemberCollectionView';
+import OkModalView from './OkModalView';
 
 const GuildInfoView = common.View.extend({
+  el: '#guild-body',
   template,
+  events: {
+    'click #guild-out-btn': 'guildOut',
+  },
   onInitialize() {
     this.addRegion('guild_member', '#guild-member');
   },
@@ -12,6 +20,22 @@ const GuildInfoView = common.View.extend({
       'guild_member',
       new GuildMemberCollectionView({ model: this.model }),
     );
+  },
+  guildOut() {
+    const userId = Radio.channel('login').request('get').id;
+    console.log('tt');
+    $.ajax({
+      type: 'DELETE',
+      url: `/api/guilds/${this.model.get('id')}/members/${userId}`,
+      headers: auth.getTokenHeader(),
+      success() {
+        new OkModalView().show('Success', 'Successfully Leave the guild');
+        Radio.channel('guild').request('reRender');
+      },
+      error(res) {
+        Radio.channel('error').request('trigger', res.responseText);
+      },
+    });
   },
 });
 
