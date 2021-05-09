3ba863d4bb4a2441e939004faded8911217cc442
kms <kmslkh@naver.com>
2021-05-09 16:08:58 +0900

    [ADD] add Guild Declare Base Template
    DatePicker 설치 , guild war 관련 api 완성 후 추가 구현 해야함

--- pong/app/javascript/srcs/views/GuildManageView.js
+++ pong/app/javascript/srcs/views/GuildManageView.js
@@ -2,7 +2,10 @@ import common from '../common';
 import template from '../templates/GuildManageView.html';
 
 const GuildManageView = common.View.extend({
+  el: '#guild-body',
   template,
+  events: {},
+  onInitialize() {},
   onRender() {},
 });
 
