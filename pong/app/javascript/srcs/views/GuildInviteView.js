007b3cf3442e42435a867b76c15c105efd233995
kms <kmslkh@naver.com>
2021-05-10 18:38:43 +0900

    [UPDATE] add invite Accept, Refuse

--- pong/app/javascript/srcs/views/GuildInviteView.js
+++ pong/app/javascript/srcs/views/GuildInviteView.js
@@ -1,9 +1,44 @@
+import Radio from 'backbone.radio';
+import $ from 'jquery/src/jquery';
 import common from '../common';
 import template from '../templates/GuildInviteView.html';
+import auth from '../utils/auth';
 
 const GuildInviteView = common.View.extend({
   template,
+  events: {
+    'click .o-button': 'inviteAccept',
+    'click .x-button': 'inviteRefuse',
+  },
   onRender() {},
+  inviteAccept() {
+    const user = Radio.channel('login').request('get');
+    $.ajax({
+      type: 'PUT',
+      url: `/api/users/${user.get('id')}/invites/${this.model.get('id')}`,
+      headers: auth.getTokenHeader(),
+      success() {
+        Radio.channel('guild').request('reRender');
+      },
+      error(res) {
+        Radio.channel('error').request('trigger', res.responseText);
+      },
+    });
+  },
+  inviteRefuse() {
+    const user = Radio.channel('login').request('get');
+    $.ajax({
+      type: 'DELETE',
+      url: `/api/users/${user.get('id')}/invites/${this.model.get('id')}`,
+      headers: auth.getTokenHeader(),
+      success() {
+        Radio.channel('guild').request('reRender');
+      },
+      error(res) {
+        Radio.channel('error').request('trigger', res.responseText);
+      },
+    });
+  },
 });
 
 export default GuildInviteView;
