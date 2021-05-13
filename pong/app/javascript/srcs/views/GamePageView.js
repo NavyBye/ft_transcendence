c9a07139ed0c764add2422a5d3861ad35f709e0c
kms <kmslkh@naver.com>
2021-05-13 11:24:17 +0900

    [ADD] add GamePage base

---
+++ pong/app/javascript/srcs/views/GamePageView.js
@@ -0,0 +1,11 @@
+import common from '../common';
+import template from '../templates/GamePageView.html';
+
+const GamePageView = common.View.extend({
+  template,
+  events: {},
+  onInitialize() {},
+  onRender() {},
+});
+
+export default GamePageView;
