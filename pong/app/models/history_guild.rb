2f82890f5330a9a7fabb32a6f8ac7d90f936714a
goyarn <goyarn1254@gmail.com>
2021-05-19 16:59:07 +0900

    [ADD] war-history associations and fixture

--- pong/app/models/history_guild.rb
+++ pong/app/models/history_guild.rb
@@ -1,2 +1,5 @@
 class HistoryGuild < ApplicationRecord
+  # associations
+  belongs_to :guild, class_name: "Guild", foreign_key: :guild_id, inverse_of: :war_history_relations
+  belongs_to :war_history, class_name: "WarHistory", foreign_key: :war_history_id, inverse_of: :war_history_relations
 end
--- pong/app/models/war_history.rb
+++ pong/app/models/war_history.rb
@@ -1,2 +1,6 @@
 class WarHistory < ApplicationRecord
+  # associations
+  has_many :war_history_relations, class_name: "HistoryGuild", inverse_of: :war_history, foreign_key: :war_history_id,
+                                   dependent: :destroy
+  has_many :guilds, through: :war_history_relations, source: :guild
 end
