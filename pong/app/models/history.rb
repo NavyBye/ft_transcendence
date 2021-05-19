7c11362e0d001b497e2ee29ca90ac2820f442337
goyarn <goyarn1254@gmail.com>
2021-05-19 14:55:59 +0900

    [ADD] history - user associations

--- pong/app/models/history.rb
+++ pong/app/models/history.rb
@@ -1,2 +1,6 @@
 class History < ApplicationRecord
+  # associations
+  has_many :history_relations, class_name: "HistoryUser", inverse_of: :history, foreign_key: :history_id,
+                               dependent: :destroy
+  has_many :users, through: :history_relations, source: :user
 end
--- pong/app/models/history_user.rb
+++ pong/app/models/history_user.rb
@@ -1,2 +1,5 @@
 class HistoryUser < ApplicationRecord
+  # associations
+  belongs_to :user, class_name: "User", foreign_key: :user_id, inverse_of: :history_relations
+  belongs_to :history, class_name: "History", foreign_key: :history_id, inverse_of: :history_relations
 end
