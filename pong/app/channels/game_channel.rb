9efbd7a3e195f62421111febe8bb95e7056556d1
DiceMono <hyekim@student.42seoul.kr>
2021-05-19 18:01:03 +0900

    [UPDATE] update condition to subscribe

--- pong/app/channels/game_channel.rb
+++ pong/app/channels/game_channel.rb
@@ -3,7 +3,7 @@ class GameChannel < ApplicationCable::Channel
     @game = Game.find params[:id]
     @host = @game.game_players.where(is_host: true).first!
     stream_for @game
-    stream_for @host
+    stream_for @host if current_user.id == @host.user_id
   end
 
   def unsubscribed; end
