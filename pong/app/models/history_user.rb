32d1a99806b49591ccfd1385c89600e1f3b50edc
goyarn <goyarn1254@gmail.com>
2021-05-19 15:03:48 +0900

    [ADD] model validations

--- pong/app/models/history_user.rb
+++ pong/app/models/history_user.rb
@@ -2,4 +2,8 @@ class HistoryUser < ApplicationRecord
   # associations
   belongs_to :user, class_name: "User", foreign_key: :user_id, inverse_of: :history_relations
   belongs_to :history, class_name: "History", foreign_key: :history_id, inverse_of: :history_relations
+
+  # validations
+  validates :user_id, uniqueness: { scope: :history_id }
+  validates :score, inclusion: { in: 0..3 }
 end
