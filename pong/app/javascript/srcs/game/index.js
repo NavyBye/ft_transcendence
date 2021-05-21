e31225f4f899eb7bfa92d0ac076d432f9684a67a
Hyeonggon Yoo <42.hyeyoo@gmail.com>
2021-05-21 16:46:29 +0900

    [UPDATE] rename to game in index.js

--- pong/app/javascript/srcs/game/index.js
+++ pong/app/javascript/srcs/game/index.js
@@ -1,13 +1,12 @@
 // module 폴더(.)에 있는 모든 js 파일 로드, 하위폴더는 없으므로 false.
 const modules = require.context('.', false, /\.js$/);
 
-const model = {};
-
+const game = {};
 modules.keys().forEach(filename => {
   if (filename !== './index.js') {
     const moduleName = filename.replace(/(\.\/|\.js)/g, '');
-    model[moduleName] = modules(filename).default;
+    game[moduleName] = modules(filename).default;
   }
 });
 
-export default model;
+export default game;
