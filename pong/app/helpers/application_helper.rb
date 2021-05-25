35a4eeb9b178af366411eebc03b87943e3993840
goyarn <goyarn1254@gmail.com>
2021-05-25 16:35:21 +0900

    [FIX] check_signal_format to symbol

--- pong/app/helpers/application_helper.rb
+++ pong/app/helpers/application_helper.rb
@@ -12,7 +12,7 @@ module ApplicationHelper
   end
 
   def send_signal(id, data)
-    # check_signal_format data
+    check_signal_format data
     user = User.find id
     SignalChannel.broadcast_to user, data
   end
@@ -20,12 +20,12 @@ module ApplicationHelper
   private
 
   def check_signal_format(data)
-    type = data['type']
+    type = data[:type]
     raise SignalChannel::InvalidFormat unless %w[connect fetch refuse].include?(type)
 
-    raise SignalChannel::InvalidFormat if type == 'connect' && data['game_id'].nil?
+    raise SignalChannel::InvalidFormat if type == 'connect' && data[:game_id].nil?
 
-    raise SignalChannel::InvalidFormat if type == 'fetch' && data['element'].nil?
+    raise SignalChannel::InvalidFormat if type == 'fetch' && data[:element].nil?
 
     true
   end
