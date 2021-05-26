5a1bc4c7db572fda97a50df55ce90e8e3d5d77df
kms <kmslkh@naver.com>
2021-05-26 20:27:15 +0900

    [FIX] fix guild war war declare버튼 누르고 OK modal 띄워주기 War declare refuse, accept이후 rerender

--- pong/app/javascript/srcs/views/GuildWarCollectionView.js
+++ pong/app/javascript/srcs/views/GuildWarCollectionView.js
@@ -1,3 +1,4 @@
+import Radio from 'backbone.radio';
 import collection from '../collections';
 import common from '../common';
 import template from '../templates/GuildWarCollectionView.html';
@@ -8,6 +9,17 @@ const GuildWarCollectionView = common.CollectionView.extend({
   childContainer: '#guild-war-collection',
   ViewType: GuildWarView,
   CollectionType: collection.GuildWarCollection,
+  onInitialize() {
+    const self = this;
+    Radio.channel('guildwar').reply('refresh', function refresh() {
+      self.collection.fetch({
+        reset: true,
+        success() {
+          self.reRender();
+        },
+      });
+    });
+  },
   onRender() {},
   afterAdd() {},
 });
