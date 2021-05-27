4fa8384dd890ca09e02f89fa7eaceaed2d0aa647
goyarn <goyarn1254@gmail.com>
2021-05-27 19:48:47 +0900

    [FIX] sign_in fail response body now returns right JSON.

--- pong/lib/custom_failure.rb
+++ pong/lib/custom_failure.rb
@@ -2,6 +2,6 @@ class CustomFailure < Devise::FailureApp
   def http_auth
     super
     self.content_type = "application/json"
-    self.response_body = { type: 'message', message: 'LOGIN FAILURE' }.to_s
+    self.response_body = { type: 'message', message: 'login failed. wrong email or password!' }.to_json
   end
 end
