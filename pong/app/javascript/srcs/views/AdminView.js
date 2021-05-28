310d59c211778e11ee436467b2bf4bd5d22d9390
kms <kmslkh@naver.com>
2021-05-28 17:49:39 +0900

    [UPDATE] admin template, view

--- pong/app/javascript/srcs/views/AdminView.js
+++ pong/app/javascript/srcs/views/AdminView.js
@@ -1,13 +1,37 @@
 import common from '../common';
 import template from '../templates/AdminView.html';
+import AdminUserCollectionView from './AdminUserCollectionView';
+import AdminGuildCollectionView from './AdminGuildCollectionView';
+import AdminTournamentView from './AdminTournamentView';
 
-const AdminView = common.View.extend({
+const YesGuildView = common.View.extend({
   el: '#content',
   template,
   events: {
-    'click #admin .submit-button': 'sendAuthCode',
+    'click .nav-item': 'showTab',
   },
-  onRender() {},
+  onInitialize() {
+    this.addRegion('adminContent', '#admin-content');
+  },
+  onRender() {
+    this.currentTab = 'user-tab';
+    this.getRegion('adminContent').show(new AdminUserCollectionView());
+  },
+  showTab(event) {
+    event.preventDefault();
+    const target = event.target.getAttribute('id');
+    this.$(`#${this.currentTab}`).removeClass('active');
+    this.$(`#${target}`).addClass('active');
+    this.currentTab = target;
+
+    if (target === 'user-tab') {
+      this.getRegion('adminContent').show(new AdminUserCollectionView());
+    } else if (target === 'guild-tab') {
+      this.getRegion('adminContent').show(new AdminGuildCollectionView());
+    } else {
+      this.getRegion('adminContent').show(new AdminTournamentView());
+    }
+  },
 });
 
-export default AdminView;
+export default YesGuildView;
