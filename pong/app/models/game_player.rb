1224b05437256e145e96e9cd7583430550854257
goyarn <goyarn1254@gmail.com>
2021-05-28 18:57:28 +0900

    [FIX] game_queue and game_player uniqueness validation message

--- pong/app/models/game_player.rb
+++ pong/app/models/game_player.rb
@@ -4,7 +4,7 @@ class GamePlayer < ApplicationRecord
   belongs_to :game, class_name: "Game", foreign_key: :game_id, inverse_of: :game_players
 
   # validations
-  validates :user_id, uniqueness: true
+  validates :user_id, uniqueness: { message: 'cannot wait 2 or more games at the same time.' }
 
   # methods
   def status_update(status)
