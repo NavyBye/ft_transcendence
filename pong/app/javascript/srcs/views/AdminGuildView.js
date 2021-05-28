514f5347ca9a901f9b3bb7a65d6b9b19418bd1f4
kms <kmslkh@naver.com>
2021-05-28 17:54:26 +0900

    [ADD] add guild manage in Admin Page - 기드 목록 보여준것 까지만 완료

---
+++ pong/app/javascript/srcs/views/AdminGuildView.js
@@ -0,0 +1,12 @@
+import common from '../common';
+import template from '../templates/AdminGuildView.html';
+
+const AdminGuildView = common.View.extend({
+  template,
+  tagName: 'tr',
+  className: 'row',
+  onInitialize() {},
+  onRender() {},
+});
+
+export default AdminGuildView;
