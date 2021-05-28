71bf7d76c560ed3bfd611c4371da0d4e419e76f2
goyarn <goyarn1254@gmail.com>
2021-05-28 14:11:00 +0900

    [FIX] for additional signal

--- pong/app/models/game_queue.rb
+++ pong/app/models/game_queue.rb
@@ -28,9 +28,11 @@ class GameQueue < ApplicationRecord
 
     queue = GameQueue.create!(params)
     # TODO : request queue time out duration to variable or const.
-    QueueTimeoverJob.set(wait: 30).perform_later queue.id if %w[ladder_tournament friendly].include?(params[:game_type])
+    if %w[ladder_tournament friendly].include?(params[:game_type])
+      QueueTimeoverJob.set(wait: 30).perform_later queue.id
+      User.find(params[:target_id]).status_update('ready')
+    end
     User.find(params[:user_id]).status_update('ready')
-    User.find(params[:target_id]).status_update('ready') if %w[ladder_tournament friendly].include?(params[:game_type])
   end
 
   def self.pop_and_match(params, cur_user_id)
