574cc63490201acf432e971678074377697d16e8
Hyeonggon Yoo <42.hyeyoo@gmail.com>
2021-05-28 16:30:56 +0900

    [ADD] add RequestPongMatchModalView
    When someone request pong match to you, you should reply it.
    So add a modal view for this.

---
+++ pong/app/javascript/srcs/views/RequestPongMatchModalView.js
@@ -0,0 +1,40 @@
+import $ from 'jquery/src/jquery';
+import common from '../common';
+import auth from '../utils/auth';
+
+const RequestPongMatchModalView = common.View.extend({
+  el: '#request-pong-match-modal-view',
+  events: {
+    'click .accept-button': 'accept',
+    'click .refuse-button': 'refuse',
+  },
+  onInitialize(obj) {
+    this.game = obj.game;
+    this.user = obj.user;
+    $(this.el).modal('show');
+  },
+  accept() {
+    $.ajax({
+      type: 'POST',
+      url: '/api/games',
+      headers: auth.getTokenHeader(),
+      data: {
+        type: 'friendly',
+      },
+    });
+    $(this.el).modal('hide');
+  },
+  refuse() {
+    $.ajax({
+      type: 'DELETE',
+      url: '/api/games/cancel',
+      headers: auth.getTokenHeader(),
+      data: {
+        type: 'friendly',
+      },
+    });
+    $(this.el).modal('hide');
+  },
+});
+
+export default RequestPongMatchModalView;
