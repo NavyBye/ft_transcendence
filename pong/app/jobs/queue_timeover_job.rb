d5a9a571799d9f63b8c41758fef6c45dfd8668ae
goyarn <goyarn1254@gmail.com>
2021-05-28 17:15:25 +0900

    [FIX] queue_timeover_job typo error

--- pong/app/jobs/queue_timeover_job.rb
+++ pong/app/jobs/queue_timeover_job.rb
@@ -5,7 +5,7 @@ class QueueTimeoverJob < ApplicationJob
     return if GameQueue.where(id: queue_id).empty?
 
     queue = GameQueue.where(id: queue_id).first!
-    requested_user = queue.user_id
+    requested_user = User.find queue.user_id
     queue.destroy!
 
     requested_user.status_update('online') if requested_user.reload.status != 'offline'
