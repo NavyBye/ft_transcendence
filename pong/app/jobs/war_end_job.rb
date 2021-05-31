c38af417e0681d273865ee14dc3b6179dacd4b37
goyarn <goyarn1254@gmail.com>
2021-05-31 20:12:46 +0900

    [REFACTOR] by rubocop

--- pong/app/jobs/war_end_job.rb
+++ pong/app/jobs/war_end_job.rb
@@ -3,7 +3,8 @@ class WarEndJob < ApplicationJob
 
   def perform(war_id)
     @war = War.find war_id
-    @war_history = WarHistory.create!(is_extended: war.is_extended, is_addon: war.is_addon, prize_point: war.prize_point)
+    @war_history = WarHistory.create!(is_extended: war.is_extended, is_addon: war.is_addon,
+                                      prize_point: war.prize_point)
     make_history_guild
     war.destroy!
   end
@@ -13,12 +14,12 @@ class WarEndJob < ApplicationJob
   def make_history_guild
     @war.guilds do |guild|
       result = if guild.war_relation.war_point == guild.war_relation.opposite.war_point
-        'draw'
-      elsif guild.war_relation.war_point > guild.war_relation.opposite.war_point
-        'win'
-      else
-        'lose'
-      end
+                 'draw'
+               elsif guild.war_relation.war_point > guild.war_relation.opposite.war_point
+                 'win'
+               else
+                 'lose'
+               end
       HistoryGuild.create!(war_history_id: @war_history.id, guild_id: guild.id, result: result)
     end
   end
