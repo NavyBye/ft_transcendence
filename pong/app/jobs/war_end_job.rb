5ad037db68a8e68875a942731e902737d44fd888
goyarn <goyarn1254@gmail.com>
2021-05-31 20:11:25 +0900

    [ADD] make war_history and history_guild when war is ended

--- pong/app/jobs/war_end_job.rb
+++ pong/app/jobs/war_end_job.rb
@@ -2,8 +2,24 @@ class WarEndJob < ApplicationJob
   queue_as :default
 
   def perform(war_id)
-    # TODO : make history...
-    war = War.find war_id
+    @war = War.find war_id
+    @war_history = WarHistory.create!(is_extended: war.is_extended, is_addon: war.is_addon, prize_point: war.prize_point)
+    make_history_guild
     war.destroy!
+  end
+
+  private
+
+  def make_history_guild
+    @war.guilds do |guild|
+      result = if guild.war_relation.war_point == guild.war_relation.opposite.war_point
+        'draw'
+      elsif guild.war_relation.war_point > guild.war_relation.opposite.war_point
+        'win'
+      else
+        'lose'
+      end
+      HistoryGuild.create!(war_history_id: @war_history.id, guild_id: guild.id, result: result)
+    end
   end
 end
