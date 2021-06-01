7a660ddd8d1822e31061b07ee9eb608a3dddc128
kms <kmslkh@naver.com>
2021-06-02 05:11:25 +0900

    [UPDATE] dm signal 처리 동작 구현

--- pong/app/javascript/srcs/views/DmRoomCollectionView.js
+++ pong/app/javascript/srcs/views/DmRoomCollectionView.js
@@ -1,3 +1,4 @@
+import Radio from 'backbone.radio';
 import collection from '../collections';
 import common from '../common';
 import template from '../templates/DmRoomCollectionView.html';
@@ -8,6 +9,19 @@ const DmRoomCollectionView = common.CollectionView.extend({
   childContainer: '#dmroom-collection',
   ViewType: DmRoomView,
   CollectionType: collection.DmRoomCollection,
+  onInitialize() {
+    const dmtab = document.getElementById('dm-notify');
+    dmtab.style.display = 'none';
+  },
+  afterAdd() {
+    const dmNotify = Radio.channel('dm').request('get');
+    dmNotify.forEach(dmRoomId => {
+      const dmRoom = document.getElementById(`${dmRoomId}-notify`);
+      if (dmRoom) {
+        dmRoom.style.display = 'inline-block';
+      }
+    });
+  },
 });
 
 export default DmRoomCollectionView;
