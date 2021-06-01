ed504a1cfdb5e47b6ed84b839bcd4125936f1cc6
DiceMono <hyekim@student.42seoul.kr>
2021-06-01 21:53:20 +0900

    [ADD] add tournament controller

---
+++ pong/app/controllers/api/tournaments_controller.rb
@@ -0,0 +1,32 @@
+module Api
+  class TournamentsController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+      tournament = Tournament.first
+      if tournament.nil?
+        render json: {}, status: :ok
+      else
+        render json: serialize(tournament), status: :ok
+      end
+    end
+
+    def create
+      raise Tournament::PermissionDenied if current_user.user?
+
+      tournament = Tournament.create! tournament_params
+      render json: serialize(tournament), status: :created
+    end
+
+    private
+
+    def serialize(tournament)
+      data = tournament.as_json
+      data["participants_count"] = tournament.participants.count
+    end
+
+    def tournament_params
+      params.permit :title, :max_participants, :is_ladder, :addon, :start_at
+    end
+  end
+end
