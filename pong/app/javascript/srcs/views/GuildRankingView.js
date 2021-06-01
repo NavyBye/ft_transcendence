d34746309a90c0dff231b3762df54c926b142aab
kms <kmslkh@naver.com>
2021-06-02 05:51:21 +0900

    [ADD] add Guild History Modal

--- pong/app/javascript/srcs/views/GuildRankingView.js
+++ pong/app/javascript/srcs/views/GuildRankingView.js
@@ -1,9 +1,17 @@
+/* eslint-disable no-new */
 import common from '../common';
 import template from '../templates/GuildRankingView.html';
+import GuildHistoryModalView from './GuildHistoryModalView';
 
 const GuildRankingView = common.View.extend({
   template,
+  events: {
+    'click .rank-text': 'showHistory',
+  },
   onRender() {},
+  showHistory() {
+    new GuildHistoryModalView({ model: this.model });
+  },
 });
 
 export default GuildRankingView;
