6826b5686c361d64e4ebd3e8648191ad2abbb036
goyarn <goyarn1254@gmail.com>
2021-06-02 17:21:55 +0900

    [ADD] stats to api/users/me and api/users/:id (show)

--- pong/app/controllers/api/users_controller.rb
+++ pong/app/controllers/api/users_controller.rb
@@ -15,12 +15,12 @@ module Api
 
     def show
       @user = User.find(params[:id])
-      render json: @user, status: :ok
+      render json: @user.as_json(methods: :stats), status: :ok
     end
 
     def me
       @user = User.find(current_user.id)
-      render json: @user, status: :ok
+      render json: @user.as_json(methods: :stats), status: :ok
     end
 
     def update
