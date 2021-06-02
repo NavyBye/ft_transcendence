7087e525c89d75bbabd826ecd6533eb3772bee4d
Hyeonggon Yoo <42.hyeyoo@gmail.com>
2021-06-02 09:02:21 +0900

    [ADD] fetch war info in HomeView

--- pong/app/javascript/srcs/views/HomeView.js
+++ pong/app/javascript/srcs/views/HomeView.js
@@ -1,9 +1,28 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable prefer-destructuring */
+import $ from 'jquery/src/jquery';
 import common from '../common';
 import template from '../templates/HomeView.html';
+import auth from '../utils/auth';
 
 const HomeView = common.View.extend({
   template,
-  onRender() {},
+  onRender() {
+    $.ajax({
+      type: 'GET',
+      url: '/api/wars',
+      headers: auth.getTokenHeader(),
+      success(data) {
+        if (data.length > 0) {
+          data = data[0];
+          const text = `it's war time. <br>
+          ${data.guilds[0].name} vs ${data.guilds[1].name}`;
+          $('#guild-war-text').text(text);
+          $('#guild-war-img').attr('src', '/images/war.png');
+        }
+      },
+    });
+  },
 });
 
 export default HomeView;
