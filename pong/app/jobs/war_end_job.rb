f67fa2c2dc5d14d7074137f1f35ba607c8d80ba8
goyarn <goyarn1254@gmail.com>
2021-06-03 14:19:57 +0900

    [FIX] history_guild making

--- pong/app/jobs/war_end_job.rb
+++ pong/app/jobs/war_end_job.rb
@@ -3,6 +3,7 @@ class WarEndJob < ApplicationJob
 
   def perform(war_id)
     return true if War.where(id: war_id).empty?
+
     @war = War.find war_id
     @war_history = WarHistory.create!(is_extended: @war.is_extended, is_addon: @war.is_addon,
                                       prize_point: @war.prize_point)
@@ -13,15 +14,15 @@ class WarEndJob < ApplicationJob
   private
 
   def make_history_guild
-    @war.guilds do |guild|
-      result = if guild.war_relation.war_point == guild.war_relation.opposite.war_point
-                 'draw'
-               elsif guild.war_relation.war_point > guild.war_relation.opposite.war_point
-                 'win'
-               else
-                 'lose'
-               end
-      HistoryGuild.create!(war_history_id: @war_history.id, guild_id: guild.id, result: result)
+    @war.guilds.each do |guild|
+      my_point = guild.war_relation.war_point
+      op_point = WarGuild.where('war_id = ? AND guild_id != ?', @war.id, guild.id).first!.war_point
+      @result = if my_point == op_point
+                  'draw'
+                else
+                  my_point > op_point ? 'win' : 'lose'
+                end
+      HistoryGuild.create!(war_history_id: @war_history.id, guild_id: guild.id, result: @result)
     end
   end
 end
