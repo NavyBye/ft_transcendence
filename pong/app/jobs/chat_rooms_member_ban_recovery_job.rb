e27c91de6238c1140c7abfe3e92849770e4e13f5
DiceMono <hyekim@student.42seoul.kr>
2021-06-03 20:44:27 +0900

    [UPDATE] update recovey job

--- pong/app/jobs/chat_rooms_member_ban_recovery_job.rb
+++ pong/app/jobs/chat_rooms_member_ban_recovery_job.rb
@@ -2,8 +2,18 @@ class ChatRoomsMemberBanRecoveryJob < ApplicationJob
   queue_as :default
 
   def perform(chat_rooms_member_id, ban_at)
-    return unless ChatRoomsMember.find(chat_rooms_member_id).ban_at == ban_at
+    chat_rooms_member = ChatRoomsMember.find(chat_rooms_member_id)
+    return unless chat_rooms_member.ban_at == ban_at
 
     ChatRoomsMember.update chat_rooms_member_id, ban_at: nil
+    broadcast_notification chat_rooms_member.chat_room, chat_rooms_member.user,
+                           "#{chat_rooms_member.user.nickname} is now free"
+  end
+
+  def broadcast_notification(room, target_user, message)
+    ChatRoomChannel.broadcast_to(room,
+                                 { type: "notification",
+                                   data: { body: message,
+                                           user: { id: target_user.id, nickname: target_user.nickname } } })
   end
 end
--- pong/app/jobs/chat_rooms_member_mute_recovery_job.rb
+++ pong/app/jobs/chat_rooms_member_mute_recovery_job.rb
@@ -2,8 +2,18 @@ class ChatRoomsMemberMuteRecoveryJob < ApplicationJob
   queue_as :default
 
   def perform(chat_rooms_member_id, mute_at)
-    return unless ChatRoomsMember.find(chat_rooms_member_id).mute_at == mute_at
+    chat_rooms_member = ChatRoomsMember.find(chat_rooms_member_id)
+    return unless chat_rooms_member.mute_at == mute_at
 
     ChatRoomsMember.update chat_rooms_member_id, mute_at: nil
+    broadcast_notification chat_rooms_member.chat_room, chat_rooms_member.user,
+                           "#{chat_rooms_member.user.nickname} is now free"
+  end
+
+  def broadcast_notification(room, target_user, message)
+    ChatRoomChannel.broadcast_to(room,
+                                 { type: "notification",
+                                   data: { body: message,
+                                           user: { id: target_user.id, nickname: target_user.nickname } } })
   end
 end
